caa350f141619ef716f49dc445e4e742f3282db4
AJ Grant <77135564+ajgrant6@users.noreply.github.com>
2024-06-03 14:25:04 -0700

    first commit

---
+++ src/components/AboutSection.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <section-title title="About" bgColor="#F4D06F" />
+    </div>
+    <div>
+        <image-of-me />
+    </div>
+</template>
+
+<script></script>
+
+<style></style>
---
+++ src/components/TitleScreen.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="title-container">
+    <title-item />
+  </div>
+</template>
+
+<script></script>
+
+<style scoped>
+.title-container {
+  height: 100vh; /* Full height of the viewport */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
