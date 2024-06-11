2cc73642f2b41b610a5b4fbb97a3e1db1d61d22e
AJ Grant <77135564+ajgrant6@users.noreply.github.com>
2024-06-10 19:10:08 -0700

    Fixed title line spacing for mobile view

--- src/components/MyTitle.vue
+++ src/components/MyTitle.vue
@@ -1,31 +1,32 @@
 <template>
-  <div>
-    <p>Hello, my name is</p>
-    <h1>AJ Grant</h1>
-    <p>I'm a Software Developer who's crazy for ML and AI</p>
-  </div>
+    <div>
+        <p>Hello, my name is</p>
+        <h1>AJ Grant</h1>
+        <p>I'm a Software Developer who's crazy for ML and AI</p>
+    </div>
 </template>
 
 <script></script>
 
 <style scoped>
 div {
-  /* display: flex; */
-  justify-content: center;
-  text-align: center;
-  /* font-family: 'Libre Baskerville'; */
-  font-family: 'PlayFair';
+    /* display: flex; */
+    justify-content: center;
+    text-align: center;
+    /* font-family: 'Libre Baskerville'; */
+    font-family: 'PlayFair';
 }
 
-p{
-  font-size: 24px;
-  margin-top: 0;
-  margin-bottom: 0;
+p {
+    font-size: 24px;
+    margin-top: 0;
+    margin-bottom: 0;
 }
 
 h1 {
-  font-size: 120px;
-  margin-top: -20px;
-  margin-bottom: 0px;
+    font-size: 120px;
+    margin-top: -20px;
+    margin-bottom: 0px;
+    line-height: 0.9;
 }
 </style>
