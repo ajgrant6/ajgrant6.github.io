4468b89718c9136215d94a6931c111d344539507
AJ Grant <77135564+ajgrant6@users.noreply.github.com>
2024-06-10 17:19:58 -0700

    Fixed Flex of the About section

--- src/components/AboutSection.vue
+++ src/components/AboutSection.vue
@@ -2,50 +2,44 @@
     <div>
         <section-title title="About" bgColor="#F4D06F" />
     </div>
-    <div class = "description">
-        <div class = "description-pic">
+    <div class="description-container">
+        <div class="description-item">
             <image-of-me />
         </div>
-        <div class = "description-text">
-            <p>I'm AJ Grant. I'm a recent grad from the University of California, Merced. From the latest research in Large Language Models to the newest iOS SDKs, I'm always exploring the cutting edge.</p>
+        <div class="description-item">
+            <div class="description-text">
+                <p>
+                    I'm AJ Grant. I'm a recent grad from the University of California, Merced. From
+                    the latest research in Large Language Models to the newest iOS SDKs, I'm always
+                    exploring the cutting edge.
+                </p>
+            </div>
         </div>
     </div>
 </template>
 
-<script>
-</script>
+<script></script>
 
 <style>
-
-.description {
+.description-container {
     display: flex;
+    flex-wrap: wrap;
     justify-content: center;
-    max-width: 900px;
+    max-width: 1200px;
     margin: 0 auto;
+    margin-left: 5%;
+    margin-right: 5%;
+    align-items: center; /* Center items vertically */
 }
 
-.description-pic {
-    margin-left: 20px;
-    margin-right: 20px;
+.description-item {
+    flex-grow: 1;
+    flex-basis: 300px;
 }
 
 .description-text {
-    flex: 1;
-    display: flex;
-    align-items: center;
     font-size: 18px;
-    max-width: 600px;
-    margin-left: 100px;
-    /* margin-right: 20px; */
-
+    max-width: 700px;
+    margin: 0 20px;
 }
-
-/* Media query for smaller screens */
-@media (max-width: 1000px) {
-    .description {
-        flex-direction: column;
-        align-items: center;
-    }
-}
-
 </style>
